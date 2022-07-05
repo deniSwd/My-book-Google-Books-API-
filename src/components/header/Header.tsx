03abbd765d6c8634f2ca65c91ba7447c0cb62233
deniSwd <denis_swd@mail.ru>
2022-07-05 18:11:57 +0200

    05.07.2022 deniSwd was added  some style for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
-import React, {ChangeEvent, FC, useState} from 'react';
-import s from './Header.module.scss';
-import {getBooksFromGoogle, setCategory, setSearchValue, setSorting} from "../../store/slices/myBookSlice";
-import {useAppDispatch} from "../../store/hooks";
-
+import React, {ChangeEvent, FC, useState} from 'react'
+import s from './Header.module.scss'
+import {getBooksFromGoogle, setCategory, setSearchValue, setSorting} from "../../store/slices/myBookSlice"
+import {useAppDispatch} from "../../store/hooks"
+import searchSmall from '../../assets/searchSmall.png'
+import clear from '../../assets/clear.png'
 
 export const Header: FC = () => {
 
@@ -23,30 +24,41 @@ export const Header: FC = () => {
   return (
     <header className={s.header}>
       <div className={s.headerTitle}>SEARCH for BOOKS</div>
-      <div>
+      <div className={s.inputBox}>
         <input type="text"
                onChange={searchValueChanged}
                value={searchValue}
                className={s.input}/>
-        <button onClick={onSearch}>Search</button>
-        <button onClick={() => setSearchValue('')}>Clear</button>
+        <div className={s.inputButton}>
+          <img src={clear}
+               className={s.clear}
+               onClick={() => setLocalSearchValue('')} alt='X'/>
+          <img src={searchSmall}
+               className={s.search}
+               onClick={onSearch} alt='srch'/>
+        </div>
       </div>
-      <div>
-        <label htmlFor='categories'>Categories: </label>
-        <select name="categories" id='categories' onChange={(e) => dispatch(setCategory(e.target.value))}>
-          <option value="*">all</option>
-          <option value="art">art</option>
-          <option value="biography">biography</option>
-          <option value="computers">computers</option>
-          <option value="history">history</option>
-          <option value="medical">medical</option>
-          <option value="poetry">poetry</option>
-        </select>
-        <label htmlFor='sorting'>Sorting by </label>
-        <select name='sorting' id='sorting' onChange={(e) => dispatch(setSorting(e.target.value))}>
-          <option value="relevance"> relevance</option>
-          <option value="newest">newest</option>
-        </select>
+      <div className={s.selectsWrapper}>
+        <div className={s.selectField}>
+          <div>Categories: </div>
+          <select className={s.select} name="categories" id='categories'
+                  onChange={(e) => dispatch(setCategory(e.target.value))}>
+            <option value="*">all</option>
+            <option value="art">art</option>
+            <option value="biography">biography</option>
+            <option value="computers">computers</option>
+            <option value="history">history</option>
+            <option value="medical">medical</option>
+            <option value="poetry">poetry</option>
+          </select>
+        </div>
+        <div className={s.selectField}>
+          <div>Sorting by </div>
+          <select className={s.select} name='sorting' id='sorting' onChange={(e) => dispatch(setSorting(e.target.value))}>
+            <option value="relevance"> relevance</option>
+            <option value="newest">newest</option>
+          </select>
+        </div>
       </div>
     </header>
   );
