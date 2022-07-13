7439f9173d7d7e3b7de59478d3914894cc66f73e
deniSwd <denis_swd@mail.ru>
2022-07-13 20:21:32 +0200

    13.07.2022 deniSwd was added docker

--- src/API/MyBookApi.ts
+++ src/API/MyBookApi.ts
@@ -9,7 +9,7 @@ export const userAPI = {
   async getBooks(category: string, sorting: string, searchValue: string, pageIndex: number): Promise<BooksType> {
     const startIndex = pageIndex * 30
     return instance.get<BooksType>
-    (`?q=${searchValue}+subject:${category}&orderBy=${sorting}&startIndex=${startIndex}&maxResults=30&key=AIzaSyBrHg8eoJcWhtgdFxzDxx83Ltih5zuW67w`)
+    (`?q=${searchValue}+subject:${category}&orderBy=${sorting}&startIndex=${startIndex}&maxResults=30`)
       .then(res => res.data)
   },
 }
